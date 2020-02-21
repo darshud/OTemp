6ed20d684903b68bce63a1c709b038aac9586730
darshud <darshud@users.noreply.github.com>
2020-02-20 20:04:12 -0500

    Add files via upload

--- test.js
+++ test.js
@@ -1,33 +1,19 @@
-var ball   = document.querySelector('.ball');
-var garden = document.querySelector('.garden');
-var output = document.querySelector('.output');
+function deviceOrientationHandler(e){
 
-var maxX = garden.clientWidth  - ball.clientWidth;
-var maxY = garden.clientHeight - ball.clientHeight;
+  var a = document.getElementById('alpha');
+  var b = document.getElementById('beta');
+  var g = document.getElementById('gamma');
 
-function handleOrientation(event) {
-alert(1);
-  var x = event.beta;  // In degree in the range [-180,180]
-  var y = event.gamma; // In degree in the range [-90,90]
-
-  output.innerHTML  = "beta : " + x + "\n";
-  output.innerHTML += "gamma: " + y + "\n";
-
-  // Because we don't want to have the device upside down
-  // We constrain the x value to the range [-90,90]
-  if (x >  90) { x =  90};
-  if (x < -90) { x = -90};
-
-  // To make computation easier we shift the range of 
-  // x and y to [0,180]
-  x += 90;
-  y += 90;
-
-  // 10 is half the size of the ball
-  // It center the positioning point to the center of the ball
-  ball.style.top  = (maxY*y/180 - 10) + "px";
-  ball.style.left = (maxX*x/180 - 10) + "px";
+  a.innerText = e.alpha;
+  b.innerText = e.beta;
+  g.innerText = e.gamma;
 }
 
-alert(0);
-window.addEventListener('deviceorientation', handleOrientation);
+if (window.DeviceOrientationEvent) {
+
+  window.addEventListener('deviceorientation', deviceOrientationHandler, false);
+
+}else{
+
+  console.error('Device orientation not supported in this browser.');
+}
