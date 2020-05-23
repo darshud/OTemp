e1db6dbc09a26247f8568402dbc764e017923bfd
darshud <darshud@users.noreply.github.com>
2020-05-23 19:37:35 -0400

    Create test.js

---
+++ test/test.js
@@ -0,0 +1,47 @@
+window.onload = function(){
+
+}
+
+  function permissionM() {
+
+	if ( location.protocol != "https:" ) {
+	location.href = "https:" + window.location.href.substring( window.location.protocol.length );
+	}
+
+    // feature detect
+	alert('permissionM()');
+    if (typeof DeviceMotionEvent.requestPermission === 'function') {
+      DeviceMotionEvent.requestPermission()
+        .then( function(permissionState) {
+          if (permissionState === 'granted') {
+            window.addEventListener('devicemotion', function(e) {});
+          }
+        })
+        .catch(console.error);
+    } else {
+      // handle regular non iOS 13+ devices
+    }
+  }
+
+
+  function permissionO() {
+
+	if ( location.protocol != "https:" ) {
+	location.href = "https:" + window.location.href.substring( window.location.protocol.length );
+	}
+
+    // feature detect
+	alert('permissionO()');
+    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+      DeviceOrientationEvent.requestPermission()
+        .then( function(permissionState) {
+          if (permissionState === 'granted') {
+            window.addEventListener('deviceorientation', function(e) {});
+          }
+        })
+        .catch(console.error);
+    } else {
+      // handle regular non iOS 13+ devices
+    }
+  }
+
