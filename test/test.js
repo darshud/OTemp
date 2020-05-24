d04593a6c3edbec54e8d2c0582271ea9dbe9fdeb
darshud <darshud@users.noreply.github.com>
2020-05-23 20:35:45 -0400

    Update test.js

--- test/test.js
+++ test/test.js
@@ -20,7 +20,7 @@ window.onload = function(){
       DeviceMotionEvent.requestPermission()
         .then( function(permissionState) {
           if (permissionState === 'granted') {
-            window.addEventListener('devicemotion', function(e) {alert(1);});
+            window.addEventListener('devicemotion', function(e) {});
           }
         })
         .catch(console.error);
@@ -42,7 +42,7 @@ window.onload = function(){
       DeviceOrientationEvent.requestPermission()
         .then( function(permissionState) {
           if (permissionState === 'granted') {
-            window.addEventListener('deviceorientation', function(e) {alert(2);});
+            window.addEventListener('deviceorientation', function(e) {alert(e.alpha);});
           }
         })
         .catch(console.error);
