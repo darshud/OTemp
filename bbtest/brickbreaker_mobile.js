526213809e5a6a0c377c6a6cce031cd019249dfc
darshud <darshud@users.noreply.github.com>
2020-05-24 12:21:57 -0400

    Update brickbreaker_mobile.js

--- bbtest/brickbreaker_mobile.js
+++ bbtest/brickbreaker_mobile.js
@@ -454,11 +454,11 @@ function draw()
 	
 	if(v_permitted && paddleX <canvas.width-paddleW && v_gamma > 0)
 	{
-		paddleX+=v_gamma*7;
+		paddleX=v_gamma*7;
 	}
 	else if(v_permitted && paddleX>0 && v_gamma < 0)
 	{
-		paddleX-=(v_gamma*7)*-1;
+		paddleX=(v_gamma*7)*-1;
 	}
 	
 	
